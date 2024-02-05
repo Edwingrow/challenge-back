04e2cd400b7b53e1dc3ec97547e69d77f6448c10
Edwin García <edwingrow16@gmail.com>
2024-02-05 14:48:56 -0300

    fix en el auth

--- src/app/middlewares/AuthTokenMiddleware.ts
+++ src/app/middlewares/AuthTokenMiddleware.ts
@@ -2,27 +2,26 @@ import { NextFunction } from "express";
 import { Request, Response } from "../../config/server/http";
 import { JwtAdapter } from "../../config/helpers/credentials/JwtAdapter";
 export class AuthTokenMiddleware {
-    static  validateJWT = async (req: Request, res:Response, next: NextFunction) => {
-        const autorization =  req.header('Authorization')
-       
-        if(!autorization) {
-            return res.status(400).json({
-                message: 'Token not found'
-            })
-        }
-        if(!autorization.startsWith('Bearer')) return res.status(401).json({error: true, message: 'Token invalid'})
-        const token = autorization.split(' ').at(1) || ''
-
+    static validateJWT = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const payload =  await JwtAdapter.validateToken(token)
-            if(!payload) return res.status(401).json({error: true, message: 'Token invalid'})
-           
-            next()
+          const authorization = req.header('Authorization');
+    
+          if (!authorization || !authorization.startsWith('Bearer')) {
+            return res.status(401).json({ error: true, message: 'Token not found or invalid' });
+          }
+    
+          const token = authorization.split(' ')[1] || '';
+    
+          const payload = await JwtAdapter.validateToken(token);
+    
+          if (!payload) {
+            return res.status(401).json({ error: true, message: 'Token invalid' });
+          }
+    
+          next();
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ error: 'Internal server error' });
         }
-        catch (error) {
-            console.log(error)
-            res.status(500).json({error: 'Internal server Error'})
-        }
-       
+      };
     }
-}
